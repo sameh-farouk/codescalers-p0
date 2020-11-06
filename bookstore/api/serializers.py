b2cc0406548e1d7a6b06a629d951792eef8d85ec
Sameh Abouelsaad <samehvirus@gmail.com>
2020-11-06 23:45:50 +0200

    feat: Create Serializer class for Data Model

---
+++ bookstore/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers 
+from .models import Book
+
+class TutorialSerializer(serializers.ModelSerializer):
+    # isbn,title,author_last_name,author_first_name,page_count,description
+    class Meta:
+        model = Book
+        fields = ('id',
+                  'isbn',
+                  'title',
+                  'author_last_name',
+                  'author_first_name',
+                  'page_count',
+                  'description')
+
