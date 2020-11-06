9d1d2a56abf4c3f888a0f8f5a3c21335938f2225
Sameh Abouelsaad <samehvirus@gmail.com>
2020-11-06 23:44:44 +0200

    feat: Define the Django Model Book

--- bookstore/api/models.py
+++ bookstore/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# isbn,title,author_last_name,author_first_name,page_count,description
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, blank=False, default='')
+    title = models.CharField(max_length=100, blank=False, default='')
+    author_last_name = models.CharField(max_length=50, blank=False, default='')
+    author_first_name = models.CharField(max_length=50, blank=False, default='')
+    page_count = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=500,blank=False, default='')
+    
