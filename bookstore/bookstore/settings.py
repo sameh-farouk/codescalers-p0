f69143abf709af92354e31e89d64ac73bf4a26ce
Sameh Abouelsaad <samehvirus@gmail.com>
2020-11-07 13:40:37 +0200

    style: Add a trailing comma

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'api.apps.ApiConfig',
     'django_filters',
+
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,5 @@ CORS_ORIGIN_WHITELIST = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
