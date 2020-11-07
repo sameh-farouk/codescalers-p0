0d7ae5fd320989d92ed1f9a64b1d0e7b64b0b44d
Sameh Abouelsaad <samehvirus@gmail.com>
2020-11-07 04:38:18 +0200

    chore: configure rest framework

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig',
     'corsheaders',
-
+    'api.apps.ApiConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +132,8 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8081',
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
