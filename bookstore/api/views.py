a599af8af268afa1abec5e7261c141fead8f4ce4
Sameh Abouelsaad <samehvirus@gmail.com>
2020-11-07 23:53:53 +0200

    feat: log the time taken for requests to be served

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets, mixins
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+import time
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -16,4 +17,16 @@ class BookViewSet(viewsets.ModelViewSet):
     search_fields = ['title', '=isbn']
     ordering_fields = ['isbn', 'title', 'author_first_name', 'author_last_name']
     ordering = ['id']
-    
+
+# log the time taken for each request to be served for all requests to the console    
+def started(sender, **kwargs):
+    global started
+    started = time.time()
+
+def finished(sender, **kwargs):
+    total = time.time() - started
+    print (f"request response time            | {total} seconds")
+
+from django.core.signals import request_started, request_finished
+request_started.connect(started)
+request_finished.connect(finished)
